b2ef6e2083e823d94a6999d6f0758bf9861cbf35
권준일 <wnsdlf925@naver.com>
2024-06-28 11:29:14 +0900

    chore: create project

---
+++ spring-lv1/src/test/java/com/sparta/springlv1/SpringLv1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sparta.springlv1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringLv1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
